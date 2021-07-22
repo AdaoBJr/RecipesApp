380a2a6d72c04db82a9ceb798845414c74480e83
AdaoBJr <AdaoBJr@users.noreply.github.com>
2021-07-22 14:57:45 +0000

    deploy: 036ef9938eb585719e388b37439d9c48bdbf6389

---
+++ precache-manifest.0086fa8142901275c9e7eb2337cdba01.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d50c06d7beed5068a41111ed95d4e3",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "797fe4eb611ee9ae50b2",
+    "url": "/recipes-app/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "6e3926a4086206d6debe",
+    "url": "/recipes-app/static/css/main.e14ac82f.chunk.css"
+  },
+  {
+    "revision": "797fe4eb611ee9ae50b2",
+    "url": "/recipes-app/static/js/2.9192de6f.chunk.js"
+  },
+  {
+    "revision": "18df2eab718dc17ccea497a5b21b7bd6",
+    "url": "/recipes-app/static/js/2.9192de6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e3926a4086206d6debe",
+    "url": "/recipes-app/static/js/main.735cf09b.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "96687f96a7db6359532e8e60ff9f7a3b",
+    "url": "/recipes-app/static/media/CaslonGraphique-Opti.96687f96.otf"
+  },
+  {
+    "revision": "09c1378e3297e427129a44deb1f53ec8",
+    "url": "/recipes-app/static/media/blackHeartIcon.09c1378e.svg"
+  },
+  {
+    "revision": "40f560a7c20d65ef8bdb256fa60e6ef9",
+    "url": "/recipes-app/static/media/drinkIcon.40f560a7.svg"
+  },
+  {
+    "revision": "ddadac4bdb5baf95a7f14bbeb5706f28",
+    "url": "/recipes-app/static/media/exploreIcon.ddadac4b.svg"
+  },
+  {
+    "revision": "3b021f9060ef67aea8aefeaf8baf12f0",
+    "url": "/recipes-app/static/media/fine-chefs-logo.3b021f90.svg"
+  },
+  {
+    "revision": "eb68457b3af1682badea07bdc9b22cb5",
+    "url": "/recipes-app/static/media/mealIcon.eb68457b.svg"
+  },
+  {
+    "revision": "abf21df68146c62e7c33d2b504a47610",
+    "url": "/recipes-app/static/media/profile.abf21df6.jpg"
+  },
+  {
+    "revision": "e3d9f9c6f8e6a24644c0667b9b50a9cc",
+    "url": "/recipes-app/static/media/profileIcon.e3d9f9c6.svg"
+  },
+  {
+    "revision": "1e0f141934e4191c1699c481e08aee0e",
+    "url": "/recipes-app/static/media/searchIcon.1e0f1419.svg"
+  },
+  {
+    "revision": "bb27688fe6536fd5c4336d202ed67350",
+    "url": "/recipes-app/static/media/shareIcon.bb27688f.svg"
+  },
+  {
+    "revision": "4a40663aadfe685dcc962bbc1d9333b7",
+    "url": "/recipes-app/static/media/whiteHeartIcon.4a40663a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RecipesApp/precache-manifest.489dcc15d2a8811fe2ca64f04a0f3203.js"
+  "/recipes-app/precache-manifest.0086fa8142901275c9e7eb2337cdba01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RecipesApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recipes-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
